45bc15d4dfd595d3b5fc2b9c19ebf0cafd68dff3
C.J. Winslow <whoaa512@gmail.com>
2015-08-13 17:03:54 +0200

    add example of ideal flow with explicitly creating an auth token
    generally this would work, but we run into the problem of needing to
    pass the created client object around to each promise. this doesn't
    seem scalable.
    
    it would be better if we could attach the created client back to the
    exported module, but only allow it's usage if it has been created.

---
+++ ideal-auth-token-flow.js
@@ -0,0 +1,50 @@
+var docusign = require('docusign-node')
+
+var integratorKey  = '***',                   // Integrator Key associated with your DocuSign Integration
+  email            = 'YOUR_EMAIL',            // Email for your DocuSign Account
+  password         = 'YOUR_PASSWORD',         // Password for your DocuSign Account
+  docusignEnv      = 'demo',                  // DocuSign Environment generally demo for testing purposes
+  fullName         = 'Joan Jett',             // Recipient's Full Name
+  recipientEmail   = 'joan.jett@example.com', // Recipient's Email
+  templateId       = '***',                   // ID of the Template you want to create the Envelope with
+  templateRoleName = '***',                   // Role Name of the Template
+  debug            = false;                   // Enable debug logging and debug responses from API
+
+var authCredentials = {
+  email: email,
+  password: password
+}
+
+docusign.getAPITokenAsync(authCredentials)
+  .then(function (authToken) {
+    return storeApiToken(authToken)
+  })
+  .then(function (authToken) {
+
+    var client = new docusign.Client({
+      integratorKey: integratorKey,
+      email: email,
+      password: password,
+      authToken: authToken, // optional
+      docusignEnv: docusignEnv,
+      debug: debug // optional
+    })
+
+    return client
+
+  })
+  .then(function (client) {
+    // do stuff with client
+    return client.envelopes.create(envelopeOptions)
+  })
+  .then(function (createdEnvelopeData) {
+    // how to re-use client here?
+    // Seems messy to keep passing client around.
+    // perhaps we can attach it back to the export ds module
+    console.log('createdEnvelopeData', createdEnvelopeData)
+  })
+  .catch(errHandler)
+
+function errHandler (error) {
+  console.log(error.stack || error)
+}
